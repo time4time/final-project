da624ff88665b975e45e12d726744ba64f47abf2
Laura PH <lau.p.hern@hotmail.com>
2019-03-29 14:00:01 +0100

    last min changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,10 +6,10 @@ class Footer extends Component {
         return (
             <footer className="footer">
                 <div className="container is-fluid columns">
-                    <div className="content has-text-centered column is-2">
+                    <div className="column is-2 is-vcentered">
                         <img className="footer-img" src="/logo_black.png" alt=""></img>
                     </div>
-                    <div className="content has-text-centered column">
+                    <div className="has-text-left column is-vcentered">
                         <p><strong>About us</strong></p>
                         <p>In Time 4 Time, we believe in circular economy. Modern society is created around the value of money. Our purpose is to go back to simpler times when you could enjoy your free time without having to be productive all the time. If you too are tired of the pressure of modern times, you can join us and meet new people to learn lots of new things in an environment where sharing is the key.</p>
                     </div>
