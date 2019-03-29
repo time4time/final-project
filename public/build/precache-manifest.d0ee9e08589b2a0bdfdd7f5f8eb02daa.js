38b59a5f51d50100594d87c717a0ff8d4f434faa
Ivonne Yanez Mendoza <ivonne.wftm@gmail.com>
2019-03-29 14:53:59 +0100

    ready?

---
+++ public/build/precache-manifest.d0ee9e08589b2a0bdfdd7f5f8eb02daa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3b366a2b341a226372",
+    "url": "/static/css/main.9400da99.chunk.css"
+  },
+  {
+    "revision": "1e3b366a2b341a226372",
+    "url": "/static/js/main.39bcd16f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d46e138c84099f9b102",
+    "url": "/static/css/2.89ce50c0.chunk.css"
+  },
+  {
+    "revision": "6d46e138c84099f9b102",
+    "url": "/static/js/2.4baffba9.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a2e1afda09558ad2f8b478c97c7717c7",
+    "url": "/index.html"
+  }
+];
--- routes/authentication/auth.js
+++ routes/authentication/auth.js
@@ -2,10 +2,10 @@ var express = require('express')
 var router = express.Router()
 
 
-router.get('/*', (req, res, next) => {
-    if(!req.session.user) res.status(403).json({message: "Unauthorized"})
-    else next()
-})
+// router.get('/*', (req, res, next) => {
+//     if(!req.session.user) res.status(403).json({message: "Unauthorized"})
+//     else next()
+// })
 
 // router.get('/*', (req, res, next) => {
 //     if (!req.isAuthenticated()) res.status(403).json({message: "Unauthorized"})
