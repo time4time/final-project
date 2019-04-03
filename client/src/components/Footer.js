4fce6051072d606c31483fba82b7a328d014fbb1
Laura Pascual <45874813+lauphern@users.noreply.github.com>
2019-04-03 20:00:23 +0200

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends Component {
                     </div>
                     <div className="has-text-left column is-vcentered">
                         <p><strong>About us</strong></p>
-                        <p>In Time 4 TTTTTime, we believe in circular economy. Modern society is created around the value of money. Our purpose is to go back to simpler times when you could enjoy your free time without having to be productive all the time. If you too are tired of the pressure of modern times, you can join us and meet new people to learn lots of new things in an environment where sharing is the key.</p>
+                        <p>In Time 4 Time, we believe in circular economy. Modern society is created around the value of money. Our purpose is to go back to simpler times when you could enjoy your free time without having to be productive all the time. If you too are tired of the pressure of modern times, you can join us and meet new people to learn lots of new things in an environment where sharing is the key.</p>
                     </div>
                 </div>
             </footer>
